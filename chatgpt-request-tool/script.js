59986af44ca6e93037f29333c3a35ee2c8e4b947
i-hiroyuki <short2.square@gmail.com>
2023-07-17 22:48:51 +0900

    ChatGPT リクエストツールの機能を調整

--- chatgpt-request-tool/script.js
+++ chatgpt-request-tool/script.js
@@ -1,47 +1,115 @@
-// ボタンクリック時のイベントハンドラ
-document.getElementById('generateBtn').addEventListener('click', generateAnswer);
+const genre = document.getElementById('genre');
+const generateBtn = document.getElementById('generateBtn');
+const programmingFields = document.getElementById('input-programming');
+const outputDiv = document.getElementById('output');
+const outputText = document.getElementById('output-text');
+const copyBtn = document.getElementById('copyBtn');
+let requestText = '';
+let genreValue = document.getElementById('genre').value;
+let question = document.getElementById('question').value;
+// let programmingLanguage = document.getElementById('programmingLanguage').value;
+let codeSnippet = document.getElementById('codeSnippet').value;
+let errorMessage = document.getElementById('errorMessage').value;
+let triedSolution = document.getElementById('triedSolution').value;
+let expectedResult = document.getElementById('expectedResult').value;
+let background = document.getElementById('background').value;
+let constraints = document.getElementById('constraints').value;
+
+// 生成ボタンクリック時のイベントハンドラ
+generateBtn.addEventListener('click', generateAnswer);
+
+// ジャンルの選択時のイベントハンドラ
+genre.addEventListener('change', toggleAdditionalFields);
 
 // 回答の生成関数
 function generateAnswer() {
-  const genre = document.getElementById('genre').value;
-  const question = document.getElementById('question').value;
-  const background = document.getElementById('background').value;
-  const constraints = document.getElementById('constraints').value;
-
-  if (genre === "" || question === "") {
+  genreValue = document.getElementById('genre').value;
+  question = document.getElementById('question').value;
+  
+  if (genreValue === "" || question === "") {
     const errorDiv = document.getElementById('errorDiv');
-    errorDiv.textContent = "「ジャンル」と「質問・依頼」は必須項目です。";
+    errorDiv.style.display = 'block';
     return;
   }
 
   errorDiv.style.display = 'none';
+  resetAnswer();
 
-  let requestText = '';
-  requestText += `あなたはプロの${genre}の講師です。\n以下の情報をもとに、考えられうる最高の回答を出力してください。\n`;
-  requestText += `依頼したいこと: ${question}\n`;
+  // programmingLanguage = document.getElementById('programmingLanguage').value;
+  const checkboxes = document.getElementsByName("programmingLanguage");
+  const selectedLanguages = [];
+
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedLanguages.push(checkbox.value);
+    }
+  });
+
+  console.log(selectedLanguages);
+  codeSnippet = document.getElementById('codeSnippet').value;
+  errorMessage = document.getElementById('errorMessage').value;
+  triedSolution = document.getElementById('triedSolution').value;
+  expectedResult = document.getElementById('expectedResult').value;
+  background = document.getElementById('background').value;
+  constraints = document.getElementById('constraints').value;
+
+  requestText += `あなたは${genreValue}の専門家です。\n以下の情報をもとに、最高の回答をお願いします。\n`;
+  requestText += `依頼したい内容: ${question}`;
+
+  if (genreValue === "プログラミング") {
+    if (codeSnippet || errorMessage || triedSolution || expectedResult || background || constraints) {
+      requestText += `\n詳細情報：`;
+    }
+
+    // チェックがあった場合のみ出力
+    if (selectedLanguages.length > 0) {
+      requestText += `\n・プログラミング言語： ${selectedLanguages}`;
+    }
+  
+    if (codeSnippet) {
+      requestText += `\n・コードサンプル：${codeSnippet}`;
+    }
+  
+    if (errorMessage) {
+      requestText += `\n・エラーメッセージ： ${errorMessage}`;
+    }
+  
+    if (triedSolution) {
+      requestText += `\n・試したこと： ${triedSolution}`;
+    }
+  
+    if (expectedResult) {
+      requestText += `\n・期待する結果： ${expectedResult}`;
+    }
+  }
 
   if (background) {
-    requestText += `背景情報: ${background}\n`;
+    requestText += `\n背景情報: ${background}`;
   }
 
   if (constraints) {
-    requestText += `制約や条件: ${constraints}\n`;
+    requestText += `\n制約や条件: ${constraints}`;
+  }
+
+  if (genreValue === "プログラミング") {
+    requestText += '\n以上。上記について、英語圏の情報で検討したあとに日本語で回答をお願いします。';
   }
 
   // テンプレートにその他の要素を追加
   // requestText += '[ここにその他のテンプレートを追加]\n[追加の要望や指示を記述]\n';
 
   // 生成したテキストを出力エリアに表示
-  const outputDiv = document.getElementById('output');
-  outputDiv.innerText = requestText;
+  outputText.innerText = requestText;
 
   // コピーボタンを表示
-  const copyBtn = document.getElementById('copyBtn');
   if (requestText.trim() === '') {
     copyBtn.style.display = 'none';
-  } else {
-    copyBtn.style.display = 'flex';
+    return;
   }
+
+  outputDiv.style.display = 'block';
+  copyBtn.style.display = 'flex';
+  outputDiv.scrollIntoView({ behavior: 'smooth' });
 }
 
 // コピーボタンクリック時のイベントハンドラ
@@ -49,8 +117,8 @@ document.getElementById('copyBtn').addEventListener('click', copyText);
 
 // テキストのコピー関数
 function copyText() {
-  const outputDiv = document.getElementById('output');
-  const text = outputDiv.innerText;
+  const outputText = document.getElementById('output-text');
+  const text = outputText.innerText;
 
   // テキストをクリップボードにコピー
   navigator.clipboard.writeText(text)
@@ -60,4 +128,32 @@ function copyText() {
     .catch((error) => {
       console.error('テキストのコピーに失敗しました:', error);
     });
+}
+
+// 回答内容のリセット
+function resetAnswer() {
+  programmingLanguage = '';
+  codeSnippet = '';
+  errorMessage = '';
+  triedSolution = '';
+  expectedResult = '';
+  background = '';
+  constraints = '';
+  requestText = '';
+  outputText.innerText = '';
+}
+
+// ジャンル選択時に追加の入力欄を表示/非表示する
+function toggleAdditionalFields() {
+  const genreValue = document.getElementById('genre').value;
+  outputText.innerText = '';
+  copyBtn.style.display = 'none';
+  outputDiv.style.display = 'none';
+
+  // プログラミングが選択された場合のみ追加の入力欄を表示
+  if (genreValue === "プログラミング") {
+    programmingFields.style.display = 'block';
+  } else {
+    programmingFields.style.display = 'none'; // 追加の入力欄を非表示
+  }
 }
